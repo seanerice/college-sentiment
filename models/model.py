5009155a6e60e26a9cdfad8e41219299aeb541db
seanerice <seane.rice@gmail.com>
2019-04-06 13:29:18 -0400

    Add textblob, model.py, train.py

---
+++ models/model.py
@@ -0,0 +1,29 @@
+class DataParser:
+    def __init__(self):
+        pass
+
+    def parse():
+        pass
+
+class Model:
+    def __init__(self):
+        self.data_parser
+        pass
+
+    def train(self, data):
+        pass
+
+    def test(self, data):
+        pass
+
+
+class TBSentiment(Model):
+    def __init__(self):
+        self.data_parser
+        pass
+
+    def train(self, data):
+        pass
+
+    def test(self, data):
+        pass
